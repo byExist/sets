cc7bd6b023d266589e4c32619fe18b6d4e71b6ed
Jongbeom Kwon <jongbeom.kwon@gmail.com>
2025-04-29 11:39:43 +0900

    Set 타입 노출

--- sets.go
+++ sets.go
@@ -6,18 +6,18 @@ import (
 
 var empty = struct{}{}
 
-// set represents a generic set of comparable elements.
-type set[E comparable] struct {
+// Set represents a generic Set of comparable elements.
+type Set[E comparable] struct {
 	data map[E]struct{}
 }
 
 // New creates and returns a new empty set.
-func New[E comparable]() *set[E] {
-	return &set[E]{data: make(map[E]struct{})}
+func New[E comparable]() *Set[E] {
+	return &Set[E]{data: make(map[E]struct{})}
 }
 
 // Collect creates a set from an iterator sequence.
-func Collect[E comparable](i iter.Seq[E]) *set[E] {
+func Collect[E comparable](i iter.Seq[E]) *Set[E] {
 	s := New[E]()
 	for e := range i {
 		s.data[e] = empty
@@ -26,7 +26,7 @@ func Collect[E comparable](i iter.Seq[E]) *set[E] {
 }
 
 // Clone returns a deep copy of the given set.
-func Clone[E comparable](s *set[E]) *set[E] {
+func Clone[E comparable](s *Set[E]) *Set[E] {
 	result := New[E]()
 	for e := range s.data {
 		result.data[e] = empty
@@ -35,12 +35,12 @@ func Clone[E comparable](s *set[E]) *set[E] {
 }
 
 // Len returns the number of elements in the set.
-func Len[E comparable](s *set[E]) int {
+func Len[E comparable](s *Set[E]) int {
 	return len(s.data)
 }
 
 // Contains checks if the set contains the given element.
-func Contains[E comparable](s *set[E], e E) bool {
+func Contains[E comparable](s *Set[E], e E) bool {
 	if _, ok := s.data[e]; ok {
 		return true
 	}
@@ -48,17 +48,17 @@ func Contains[E comparable](s *set[E], e E) bool {
 }
 
 // Add inserts an element into the set.
-func Add[E comparable](s *set[E], e E) {
+func Add[E comparable](s *Set[E], e E) {
 	s.data[e] = empty
 }
 
 // Remove deletes an element from the set.
-func Remove[E comparable](s *set[E], e E) {
+func Remove[E comparable](s *Set[E], e E) {
 	delete(s.data, e)
 }
 
 // Pop removes and returns an arbitrary element from the set.
-func Pop[E comparable](s *set[E]) (E, bool) {
+func Pop[E comparable](s *Set[E]) (E, bool) {
 	for e := range s.data {
 		delete(s.data, e)
 		return e, true
@@ -68,12 +68,12 @@ func Pop[E comparable](s *set[E]) (E, bool) {
 }
 
 // Clear removes all elements from the set.
-func Clear[E comparable](s *set[E]) {
+func Clear[E comparable](s *Set[E]) {
 	s.data = make(map[E]struct{})
 }
 
 // Values returns an iterator over all elements in the set.
-func Values[E comparable](s *set[E]) iter.Seq[E] {
+func Values[E comparable](s *Set[E]) iter.Seq[E] {
 	return func(yield func(E) bool) {
 		for e := range s.data {
 			if !yield(e) {
@@ -84,7 +84,7 @@ func Values[E comparable](s *set[E]) iter.Seq[E] {
 }
 
 // Equal checks if two sets contain exactly the same elements.
-func Equal[E comparable](a, b *set[E]) bool {
+func Equal[E comparable](a, b *Set[E]) bool {
 	if len(a.data) != len(b.data) {
 		return false
 	}
@@ -97,7 +97,7 @@ func Equal[E comparable](a, b *set[E]) bool {
 }
 
 // IsDisjoint checks if two sets have no elements in common.
-func IsDisjoint[E comparable](a, b *set[E]) bool {
+func IsDisjoint[E comparable](a, b *Set[E]) bool {
 	for e := range a.data {
 		if _, ok := b.data[e]; ok {
 			return false
@@ -107,7 +107,7 @@ func IsDisjoint[E comparable](a, b *set[E]) bool {
 }
 
 // IsSubset checks if the first set is a subset of the second set.
-func IsSubset[E comparable](a, b *set[E]) bool {
+func IsSubset[E comparable](a, b *Set[E]) bool {
 	if len(a.data) > len(b.data) {
 		return false
 	}
@@ -120,7 +120,7 @@ func IsSubset[E comparable](a, b *set[E]) bool {
 }
 
 // IsSuperset checks if the first set is a superset of the second set.
-func IsSuperset[E comparable](a, b *set[E]) bool {
+func IsSuperset[E comparable](a, b *Set[E]) bool {
 	if len(a.data) < len(b.data) {
 		return false
 	}
@@ -133,7 +133,7 @@ func IsSuperset[E comparable](a, b *set[E]) bool {
 }
 
 // Union returns a new set containing all elements from both sets.
-func Union[E comparable](a, b *set[E]) *set[E] {
+func Union[E comparable](a, b *Set[E]) *Set[E] {
 	result := New[E]()
 	for e := range a.data {
 		result.data[e] = empty
@@ -145,7 +145,7 @@ func Union[E comparable](a, b *set[E]) *set[E] {
 }
 
 // Intersection returns a new set containing only elements present in both sets.
-func Intersection[E comparable](a, b *set[E]) *set[E] {
+func Intersection[E comparable](a, b *Set[E]) *Set[E] {
 	result := New[E]()
 	for e := range a.data {
 		if _, ok := b.data[e]; ok {
@@ -156,7 +156,7 @@ func Intersection[E comparable](a, b *set[E]) *set[E] {
 }
 
 // Difference returns a new set containing elements in the first set but not in the second.
-func Difference[E comparable](a, b *set[E]) *set[E] {
+func Difference[E comparable](a, b *Set[E]) *Set[E] {
 	result := New[E]()
 	for e := range a.data {
 		if _, ok := b.data[e]; !ok {
@@ -167,7 +167,7 @@ func Difference[E comparable](a, b *set[E]) *set[E] {
 }
 
 // SymmetricDifference returns a new set containing elements present in either set but not in both.
-func SymmetricDifference[E comparable](a, b *set[E]) *set[E] {
+func SymmetricDifference[E comparable](a, b *Set[E]) *Set[E] {
 	result := New[E]()
 	for e := range a.data {
 		if _, ok := b.data[e]; !ok {
